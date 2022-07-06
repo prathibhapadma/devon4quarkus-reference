e23e280e96311a148074b8329af34b5bb01bf7b0
prathibhapadma <prathibha.padma@capgemini.com>
2022-07-06 16:23:22 +0530

    Update secrets.sh

--- .github/workflows/scripts/secrets.sh
+++ .github/workflows/scripts/secrets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 #Command to create secrets to pull image from private registry.
-kubectl delete secret "$2" --namespace="$1" 
+# kubectl delete secret "$2" --namespace="$1" 
 # create secrets for private registry.
 kubectl create secret docker-registry "$2" --docker-server="$5" --docker-username="$3" --docker-password="$4" --namespace="$1" 
 # export secrets name and add secrets into deployment file.
