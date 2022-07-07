ec6a9a0f93c2f2024034b1677dc3068a2627cc43
prathibhapadma <prathibha.padma@capgemini.com>
2022-07-07 16:41:52 +0530

    Update deploy.sh

--- .github/workflows/scripts/deploy.sh
+++ .github/workflows/scripts/deploy.sh
@@ -3,8 +3,10 @@ set -e
 # Add image name and ingress DNS name.
 export image="$2" dns="$3"
 yq eval '.spec.template.spec.containers[0].image = "'"$image"'"' -i "$4"
+cat "$4"
 yq eval '.spec.rules[0].host = "'"$dns"'"' -i "$5"
+cat "$5"
 # Create namespace in EKS cluster.
-kubectl create namespace "$1"
+#kubectl create namespace "$1"
 # Apply manifest files.
-kubectl apply -f "$6" --namespace="$1"
+#kubectl apply -f "$6" --namespace="$1"
