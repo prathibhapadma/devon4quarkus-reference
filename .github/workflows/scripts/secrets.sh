7ec56e11e69d62f9afa0b27eca5f6de6a617f14d
prathibhapadma <prathibha.padma@capgemini.com>
2022-07-13 14:42:53 +0530

    Update secrets.sh

--- .github/workflows/scripts/secrets.sh
+++ .github/workflows/scripts/secrets.sh
@@ -5,17 +5,10 @@ if [ $namespaceStatus == "Active" ]
 then
     echo "Deploy apps"
 else
-    echo "Create namespace"
     #Create namespace in cluster.
     kubectl create namespace "$1"
 fi
-# #Command to delete secrets to pull image from private registry.
-# secretstatus=$(kubectl get secrets "$2" --namespace="$1")
-# if [ $secretstatus == "Active" ]
-# then
-#     echo "Delete already exitst secret "$2""
-#     kubectl delete secret "$2" --namespace="$1" --ignore-not-found
-# fi
+# Command to delete secrets to pull image from private registry.
 kubectl delete secret "$2" --namespace="$1" --ignore-not-found
 kubectl create secret docker-registry "$2" --docker-server="$5" --docker-username="$3" --docker-password="$4" --namespace="$1"
 # export secrets name and add secrets into deployment file.
