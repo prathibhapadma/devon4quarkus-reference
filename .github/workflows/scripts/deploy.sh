f2c10b4a93c0a3185a3725215fa7f86b09dda2cf
prathibhapadma <prathibha.padma@capgemini.com>
2022-07-13 14:51:48 +0530

    Update deploy.sh

--- .github/workflows/scripts/deploy.sh
+++ .github/workflows/scripts/deploy.sh
@@ -3,16 +3,13 @@ set -e
 # Add image name and ingress DNS name.
 export image="$2" dns="$3"
 yq eval '.spec.template.spec.containers[0].image = "'"$image"'"' -i "$4"
-cat $4
 yq eval '.spec.rules[0].host = "'"$dns"'"' -i "$5"
-cat $5
-#kubectl delete already existed namespace
+# Deploy apps in exists namespace, If not exists Create new namespace.
 namespaceStatus=$(kubectl get ns "$1" -o json | jq .status.phase -r)
 if [ $namespaceStatus == "Active" ] 
 then
     echo "Deploy app"
 else
-    #Create namespace in cluster.
     kubectl create namespace "$1"
 fi
 # Apply manifest files.
