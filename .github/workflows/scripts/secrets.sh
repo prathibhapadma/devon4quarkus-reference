74b217e8d9b832ffe7cccf22216c71220c0ef683
prathibhapadma <prathibha.padma@capgemini.com>
2022-07-14 12:07:53 +0530

    Update secrets.sh

--- .github/workflows/scripts/secrets.sh
+++ .github/workflows/scripts/secrets.sh
@@ -3,7 +3,8 @@ set -e
 # Deploy apps in exists namespace, If not exists Create new namespace.
 kubectl get namespace | grep -q "^$1" || kubectl create namespace "$1"
 # Command to existed secrets and create new secrets to pull image from private registry.
-kubectl delete secret "$2" --namespace="$1" --ignore-not-found | kubectl create secret docker-registry "$2" --docker-server="$5" --docker-username="$3" --docker-password="$4" --namespace="$1"
+kubectl delete secret "$2" --namespace="$1" --ignore-not-found 
+kubectl create secret docker-registry "$2" --docker-server="$5" --docker-username="$3" --docker-password="$4" --namespace="$1"
 # export secrets name and add secrets into deployment file.
 export secrets="$2" 
 yq e '.spec.template.spec."imagePullSecrets"=[{"name":"secrets"}]' -i "$6" 
