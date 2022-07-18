9d5915e656b3678741488e209206ac91866765ab
prathibhapadma <prathibha.padma@capgemini.com>
2022-07-18 14:17:36 +0530

    Update deploy.sh

--- .github/workflows/scripts/deploy.sh
+++ .github/workflows/scripts/deploy.sh
@@ -3,7 +3,6 @@ set -e
 branch="$8"
 source ./"$8"
 echo "tag: ${tag}"
-
 # Add image name and ingress DNS name.
 export image="$2" tag="${tag}" dns="$4" 
 yq eval '.spec.template.spec.containers[0].image = "'"$image:$tag"'"' -i "$5"
@@ -11,5 +10,5 @@ cat "$5"
 yq eval '.spec.rules[0].host = "'"$dns"'"' -i "$6"
 cat "$6"
 # Deploy apps in exists namespace, If not exists Create new namespace and apply manifest files.
-# kubectl get namespace | grep -q "^$1" || kubectl create namespace "$1"
-# kubectl apply -f "$7" --namespace="$1"
+kubectl get namespace | grep -q "^$1" || kubectl create namespace "$1"
+kubectl apply -f "$7" --namespace="$1"
