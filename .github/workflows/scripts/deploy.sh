2442ead65209d64d0a7b92b30940f0303a596393
prathibhapadma <prathibha.padma@capgemini.com>
2022-07-18 17:31:33 +0530

    Adding the source YAML

--- .github/workflows/scripts/deploy.sh
+++ .github/workflows/scripts/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
+echo "branch=$7"
+source "$7"
+echo "tag: ${tag}"
 # Add image name and ingress DNS name.
-export image="$2" tag="$3" dns="$4" 
-yq eval '.spec.template.spec.containers[0].image = "'"$image:$tag"'"' -i "$5"
-cat "$5"
-yq eval '.spec.rules[0].host = "'"$dns"'"' -i "$6"
-cat "$6"
+export image="$2" dns="$3"
+yq eval '.spec.template.spec.containers[0].image = "'"$image"'"' -i "$4"
+yq eval '.spec.rules[0].host = "'"$dns"'"' -i "$5"
 # Deploy apps in exists namespace, If not exists Create new namespace and apply manifest files.
 kubectl get namespace | grep -q "^$1" || kubectl create namespace "$1"
-kubectl apply -f "$7" --namespace="$1"
+kubectl apply -f "$6" --namespace="$1"
