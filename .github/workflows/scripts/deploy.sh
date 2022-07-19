2f23645864758f583a759c13b281c004c2fcc28a
prathibhapadma <prathibha.padma@capgemini.com>
2022-07-19 08:24:13 +0530

    Update deploy.sh

--- .github/workflows/scripts/deploy.sh
+++ .github/workflows/scripts/deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
-echo "branch=$7"
-source "$7"
-echo "tag: ${tag}"
 # Add image name and ingress DNS name.
-export image="$2" tag=${tag} dns="$3"
+source "$7"
+export image="$2" tag="${tag}" dns="$3"
 yq eval '.spec.template.spec.containers[0].image = "'"$image:$tag"'"' -i "$4"
 cat $4
 yq eval '.spec.rules[0].host = "'"$dns"'"' -i "$5"
